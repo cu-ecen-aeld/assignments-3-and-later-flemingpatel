fbac9975f670c5404b62cca077330bf37de390c4
Fleming Patel <flpa5726@colorado.edu>
2024-09-16 19:21:12 -0400

    assignment-7-part-1-complete local tests are passed

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,27 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    size_t cumulative_offset = 0;
+    uint8_t idx = buffer->out_offs;
+
+    if (buffer == NULL || entry_offset_byte_rtn == NULL)
+    {
+        return NULL;
+    }
+
+    do
+    {
+        struct aesd_buffer_entry *entry = &buffer->entry[idx];
+        if (cumulative_offset + entry->size > char_offset)
+        {
+            *entry_offset_byte_rtn = char_offset - cumulative_offset;
+            return entry;
+        }
+
+        cumulative_offset += entry->size;
+        idx = (idx + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    } while (idx != buffer->in_offs);
+
     return NULL;
 }
 
@@ -44,9 +62,24 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    if (buffer == NULL || add_entry == NULL)
+    {
+        return;
+    }
+
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    if (buffer->full)
+    {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
 }
 
 /**
